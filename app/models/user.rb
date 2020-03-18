c32a38a93f7d4bf42f7112b0776d4e22a2f44e9f
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-18 16:29:11 -0400

    Add the appropriate associations to the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     before_save :downcase_name
+    has_many :articles, foreign_key: "author_id" ,dependent: :destroy
     validates :name, presence: true, length: {minimum: 5},
                 uniqueness: true
 
