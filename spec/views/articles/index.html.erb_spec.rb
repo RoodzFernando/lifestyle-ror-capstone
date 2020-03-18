942bb25fd9dbb22cfea92facb31f2cd1e6c4397a
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-18 16:09:34 -0400

    Generate the Article model

---
+++ spec/views/articles/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "articles/index", type: :view do
+  before(:each) do
+    assign(:articles, [
+      Article.create!(
+        :author_id => 2,
+        :title => "Title",
+        :text => "MyText"
+      ),
+      Article.create!(
+        :author_id => 2,
+        :title => "Title",
+        :text => "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of articles" do
+    render
+    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => "Title".to_s, :count => 2
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+  end
+end
---
+++ spec/views/articles/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "articles/new", type: :view do
+  before(:each) do
+    assign(:article, Article.new(
+      :author_id => 1,
+      :title => "MyString",
+      :text => "MyText"
+    ))
+  end
+
+  it "renders new article form" do
+    render
+
+    assert_select "form[action=?][method=?]", articles_path, "post" do
+
+      assert_select "input[name=?]", "article[author_id]"
+
+      assert_select "input[name=?]", "article[title]"
+
+      assert_select "textarea[name=?]", "article[text]"
+    end
+  end
+end
