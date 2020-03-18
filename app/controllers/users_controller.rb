6f81f6499e0b659be202c72717c8b07817084e42
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-18 19:36:58 -0400

    Fix the an issue in the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @users
+      redirect_to users_path
       flash[:success] = "Thanks for registering!"
     else
       render 'new'
