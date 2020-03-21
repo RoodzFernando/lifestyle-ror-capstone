71d38bbccf42d6ffad6ac803e160fc6eb06fbcc9
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-21 12:42:24 -0400

    Generate the vote controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,7 @@ class Article < ApplicationRecord
     has_one_attached :image
     has_one :category, foreign_key: "category_id"
     accepts_nested_attributes_for :category
+    has_many :votes
     validates :title, presence: true, length: {maximum: 150}
     validates :text, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     before_save :downcase_name
     has_many :articles, foreign_key: "author_id" ,dependent: :destroy
+    has_many :votes
     validates :name, presence: true, length: {minimum: 5},
                 uniqueness: true
 
