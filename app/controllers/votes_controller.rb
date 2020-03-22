e6dd78ff7088bce56dfd65a1a44ea9db50d27849
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-21 23:22:01 -0400

    Add the vote link and change the route

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,10 @@
 class VotesController < ApplicationController
+
+    def create
+        @user = User.find_by(id: current_user)
+        @article = Article.find(3)
+        @vote = Vote.create(user_id: @user.id, article_id:  @article.id)
+        redirect_to articles_path
+        flash[:success] = 'You have voted.'
+    end
 end
